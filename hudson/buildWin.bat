fee2a737f7bf6a86efa5a7f72fe4778afdbdf5b9
b.wenzel <b.wenzel@techsmith.com>
2013-07-08 16:22:32 +0000

    Modified batch file for mp4v2 for use on hudson.

--- hudson/buildWin.bat
+++ hudson/buildWin.bat
@@ -1,19 +1,25 @@
 @ECHO OFF
 
 @REM Set paths that are independent of "bitness".
-SET CMAKE_PATH=C:\Program Files (x86)\CMake 2.8\bin\cmake.exe
-SET VS_PATH=C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.com
+SET CMAKE_PATH=C:\Program Files\CMake 2.8\bin\cmake.exe
+SET VS_PATH=C:\Program Files\Microsoft Visual Studio 10.0\Common7\IDE\devenv.com
 SET SVN_PATH=C:\Program Files\TortoiseSVN\bin\svn.exe
 SET TRUNK_PATH=%~1
-SET TF_PATH=C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\tf.exe
-SET TFPT_PATH=C:\Program Files (x86)\Microsoft Team Foundation Server 2010 Power Tools\tfpt.exe
-SET TFS_PATH=C:\Software\Common\mp4v2\Development
-SET DIR_SYMSTORE=C:\Program Files\Debugging Tools for Windows (x64)
+SET TF_PATH=C:\Program Files\Microsoft Visual Studio 10.0\Common7\IDE\tf.exe
+SET TFPT_PATH=C:\Program Files\Microsoft Team Foundation Server 2010 Power Tools\tfpt.exe
+SET TFS_PATH=D:\Projects_TFS\Common\mp4v2\Development
+SET DIR_SYMSTORE=C:\Program Files\Debugging Tools for Windows (x86)
 
 SET MP4V2_INCPATH=%TRUNK_PATH%\include\mp4v2
 SET MP4V2_SRCPATH=%TRUNK_PATH%\src
 SET MP4V2_BINPATH=%TRUNK_PATH%\bin\win
 SET MP4V2_LIBPATH=%TRUNK_PATH%\lib\win
+
+@REM Update files from TFS, so that it thinks we have the latest code when we make our check-in later
+"%TF_PATH%" get "%TFS_PATH%"
+
+@REM Delete all the files in the TFS path so we can tell if a file was deleted in SVN
+del "%TFS_PATH%" /S /F /Q
 
 @REM Do the 32-bit builds.
 SET BITS=32
@@ -41,6 +47,10 @@ xcopy /S /I /Y /R "%MP4V2_BUILD_BINPATH%\Release" "%MP4V2_BINPATH%\Release\win%B
 xcopy /S /I /Y /R "%MP4V2_BUILD_LIBPATH%\Debug"   "%MP4V2_LIBPATH%\Debug\win%BITS%"
 xcopy /S /I /Y /R "%MP4V2_BUILD_LIBPATH%\Release" "%MP4V2_LIBPATH%\Release\win%BITS%"
 
+@REM Copy all the 32-bit lib/bin files into the correct TFS directory for committing
+xcopy /S /I /Y /R "%MP4V2_BUILD_BINPATH%" "%TFS_PATH%\bin\%PLATFORM%"
+xcopy /S /I /Y /R "%MP4V2_BUILD_LIBPATH%" "%TFS_PATH%\lib\%PLATFORM%"
+
 
 @REM Do the 64-bit builds.
 SET BITS=64
@@ -67,35 +77,29 @@ xcopy /S /I /Y /R "%MP4V2_BUILD_BINPATH%\Release" "%MP4V2_BINPATH%\Release\win%B
 xcopy /S /I /Y /R "%MP4V2_BUILD_LIBPATH%\Debug"   "%MP4V2_LIBPATH%\Debug\win%BITS%"
 xcopy /S /I /Y /R "%MP4V2_BUILD_LIBPATH%\Release" "%MP4V2_LIBPATH%\Release\win%BITS%"
 
+@REM Copy all the 64-bit lib/bin files into the correct TFS directory for committing
+xcopy /S /I /Y /R "%MP4V2_BUILD_BINPATH%" "%TFS_PATH%\bin\%PLATFORM%"
+xcopy /S /I /Y /R "%MP4V2_BUILD_LIBPATH%" "%TFS_PATH%\lib\%PLATFORM%"
+
 
 @REM Check in the binaries to SVN.
-@REM "%SVN_PATH%" commit "%TRUNK_PATH%" -m "Windows binaries committed by build server."
+"%SVN_PATH%" commit "%TRUNK_PATH%" -m "Windows binaries committed by build server."
+@IF %ERRORLEVEL% NEQ 0 GOTO :fail
 
-@REM @REM Update files from TFS, so that it thinks we have the latest code when we make our check-in later
-@REM "%TF_PATH%" get "%TFS_PATH%"
-@REM 
-@REM @REM Delete all the files in the TFS path so we can tell if a file was deleted in SVN
-@REM del "%TFS_PATH%" /S /F /Q
-@REM 
-@REM @REM Copy all the files into the TFS_PATH
-@REM xcopy /S /I /Y /R "%MP4V2_INCPATH%"   "%TFS_PATH%\include\TREC"
-@REM xcopy /S /I /Y /R "%MP4V2_SRCPATH%"   "%TFS_PATH%\src"
-@REM xcopy /S /I /Y /R "%MP4V2_BINPATH32%" "%TFS_PATH%\bin\Win32"
-@REM xcopy /S /I /Y /R "%MP4V2_LIBPATH32%" "%TFS_PATH%\lib\Win32"
-@REM 
-@REM xcopy /S /I /Y /R "%MP4V2_BINPATH64%" "%TFS_PATH%\bin\x64"
-@REM xcopy /S /I /Y /R "%MP4V2_LIBPATH64%" "%TFS_PATH%\lib\x64"
-@REM 
-@REM @REM Go online to pend edits including adds/deletes
-@REM @REM "%TFPT_PATH%" online "%TFS_PATH%" /recursive /adds /deletes /diff /exclude:mac,*.idb,*.exp,*.ilk,*testrunner.* /noprompt
-@REM @IF %ERRORLEVEL% NEQ 0 GOTO :fail
-@REM 
-@REM @REM Check the Windows binaries into TFS
-@REM @REM "%TF_PATH%" checkin "%TFS_PATH%" /noprompt /recursive /notes:"Code Reviewer"="none" /comment:"Automated checkin from Hudson project"
-@REM @IF %ERRORLEVEL% NEQ 0 GOTO :fail
+@REM Copy all the source/include files into the TFS_PATH
+xcopy /S /I /Y /R "%MP4V2_INCPATH%" "%TFS_PATH%\include\mp4v2"
+xcopy /S /I /Y /R "%MP4V2_SRCPATH%" "%TFS_PATH%\src"
+
+@REM Go online to pend edits including adds/deletes
+"%TFPT_PATH%" online "%TFS_PATH%" /recursive /adds /deletes /diff /exclude:mac,*.idb,*.exp,*.ilk,*testrunner.* /noprompt
+@IF %ERRORLEVEL% NEQ 0 GOTO :fail
+
+@REM Check the Windows binaries into TFS
+"%TF_PATH%" checkin "%TFS_PATH%" /noprompt /recursive /notes:"Code Reviewer"="none" /comment:"Automated checkin from Hudson project"
+@IF %ERRORLEVEL% NEQ 0 GOTO :fail
 
 @REM Store symbols on symbol server
-@REM "%DIR_SYMSTORE%\symstore.exe" add /r /f "%TFS_PATH%\*.*" /s \\techsmith.com\symbols /t "TREC"
+"%DIR_SYMSTORE%\symstore.exe" add /r /f "%TFS_PATH%\*.*" /s \\techsmith.com\symbols /t "mp4v2"
 
 GOTO :done
 
